d647fe578bfd97e775bc29490fbe89ba8990f48c
Mahesh <mahesh.cloud84@gmail.com>
2020-02-04 11:04:26 +0000

    Adding the playbook and the roles for Apache and PHP

---
+++ roles/php/files/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Welcome to DevOps on AWS</title>
+</head>
+<body>
+    <h1><?php echo "Welcome to DevOps on AWS. This text is generated using PHP</h1>
+</body>
+</html>
+
